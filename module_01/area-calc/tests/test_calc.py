6c6c16dea68f50c9848e31eca8c83ec8c7c2b42a
Chris King-Parra <chris@kingparra.work>
2024-01-30 19:41:19 -0500

    area-calc: Fix test case for area

--- module_01/area-calc/tests/test_calc.py
+++ module_01/area-calc/tests/test_calc.py
@@ -4,15 +4,16 @@ from pint import UnitRegistry
 
 from area_calc import calc
 
+
 ureg = UnitRegistry()
 Q = ureg.Quantity
 
 
 def test_area():
     """Does area do multiplication on floats?"""
-    assert calc.area("2m", "3.5m") == Q("7m")
+    assert calc.area("2m", "3.5m") == Q("7 meters squared")
 
 
 def test_perim():
     """Will perim use the right formula?"""
-    assert calc.perim("2m", "3.5m") == Q("9m")
+    assert calc.perim("2m", "3.5m") == Q("11m")
