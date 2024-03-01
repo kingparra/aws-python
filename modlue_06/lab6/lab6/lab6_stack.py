6cc6aa2d2ebbc2922c22ad8c80bb6ff6f0cbd911
Chris King-Parra <chris@kingparra.work>
2024-02-29 21:20:53 -0500

    Add asg scaling policy

--- modlue_06/lab6/lab6/lab6_stack.py
+++ modlue_06/lab6/lab6/lab6_stack.py
@@ -62,3 +62,10 @@ class Lab6Stack(Stack):
         listener.add_targets("Target", port=80, targets=[asg])
         listener.connections.allow_default_port_from_any_ipv4(
             "Open to the world")
+        asg.scale_on_request_count(
+            "AModestLoad",
+            target_requests_per_minute=60)
+        CfnOutput(self,
+                  "LoadBalancer",
+                  export_name="LoadBalancer",
+                  value=lb.load_balancer_dns_name)
