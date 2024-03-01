721aade65f27d36de29c745ef8ba11ac8239b9a3
Chris King-Parra <chris@kingparra.work>
2024-02-29 20:15:40 -0500

    Add user-data script

--- modlue_06/lab6/lab6/lab6_stack.py
+++ modlue_06/lab6/lab6/lab6_stack.py
@@ -11,9 +11,31 @@ from constructs import Construct
 import textwrap
 
 
+data = textwrap.dedent("""
+    #!/usr/bin/env bash
+    yum install httpd -y
+    systemctl enable --now httpd
+    cat > /var/www/html/index.html << EOF
+    <html>
+        <head>
+            <title> Example Web Server</title>
+        </head>
+        <body>
+            <div>
+                <center>
+                    <h2>Welcome AWS $(hostname -f)</h2>
+                    <hr/>
+                    $(curl "http://169.254.169.254/latest/meta-data/instance-id")
+                </center>
+            </div>
+        </body>
+    </html>
+    EOF
+    """)
 
 
 class Lab6Stack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         vpc = ec2.Vpc(self, "VPC")
+        user_data = ec2.UserData.for_linux().add_commands(data)
